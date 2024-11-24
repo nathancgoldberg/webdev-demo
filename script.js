394acc61b372fc7fc3437b9dd00b40f611f39eb5
nathancgoldberg <ncgoldberg@wm.edu>
2024-11-24 14:31:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
 
+function addTask(){
+  const taskInput = document.getElementById('taskInput').value;
+  alert(`New Task: ${taskInput}`);
+}
